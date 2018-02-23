241cdbd71d783849fbb817dd89e5d8a7f1eb4feb
Teemu Orava <teemu.orava@kapsi.fi>
2018-02-24 01:06:18 +0200

    Adds missing files.

---
+++ src/models/CategoryAttribute.js
@@ -0,0 +1,63 @@
+/*
+Category Attribute DB model
+
+protein
+carbohydrate
+fiber
+fat
+magnesium
+natrium
+a vitamin
+b vitamin
+c vitamin
+d vitamin
+e vitamin
+width
+length
+height
+mass
+co2
+methane
+*/
+
+import {Model} from 'objection';
+
+class CategoryAttribute extends Model {
+	static get tableName() {
+		return 'CategoryAttribute';
+	}
+
+	static get jsonSchema() {
+		return {
+			type: 'object',
+
+			properties: {
+				id: {type: 'integer'},
+				value: {type: 'float'}
+			}
+		}
+  }
+  
+  static get relationMappings() {
+		return {
+			category: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: __dirname+'/Category',
+				join: {
+					from: 'CategoryAttribute.categoryId',
+					to: 'Category.id'
+				}
+			},
+			attribute: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: __dirname+'/Attribute',
+				join: {
+					from: 'CategoryAttribute.attributeId',
+					to: 'Attribute.id'
+				}
+			}
+    }
+  }
+}
+
+module.exports = CategoryAttribute;
