b4d8e8840ac9b676041dcdc475e830aa933a7be1
Teemu Orava <teemu.orava@kapsi.fi>
2018-09-13 20:19:58 +0300

    Adds distances to TextBoxTool.

--- src/components/TextBoxTool.jsx
+++ src/components/TextBoxTool.jsx
@@ -9,7 +9,8 @@ export default class TextBoxTool extends Component {
 
     this.onChange = this.onChange.bind(this);
     this.state = {
-      boxes: []
+      boxes: [],
+      distances: []
     }
   }
   onChange(event) {
@@ -47,24 +48,35 @@ export default class TextBoxTool extends Component {
         result.innerHTML = response.data.hocr;
 
         let words = result.getElementsByClassName('ocrx_word'),
-            coords,
+            coords, distances = [],
             origwidth = img.naturalWidth,
             factor = img.width/origwidth,
             wrapper = document.getElementById('canvas-wrapper'),
             boxes = [],
             left, top, right, bottom, width, height,
+            x_sum = 0,
+            x_gravity,
+            text,
             boundaries = {};
 
         console.log(img.width, origimg.width);
 
         for (let i in words) {
+          if (!words[i].innerText || !words[i].innerText.trim()) continue;
+          words[i].coords = coords = words[i].title.split(';')[0].substring(5).split(' ');
+          x_sum+= parseInt(coords[0])+parseInt(coords[2])/2;
+        }
+
+        x_gravity = x_sum/words.length;
+
+        for (let i in words) {
           console.log(words[i].innerHTML);
           if (!words[i].innerText || !words[i].innerText.trim()) continue;
-          coords = words[i].title.split(';')[0].substring(5).split(' ');
-          left = coords[0];
-          top = coords[1];
-          right = coords[2];
-          bottom = coords[3];
+          coords = words[i].coords;
+          left = parseInt(coords[0]);
+          top = parseInt(coords[1]);
+          right = parseInt(coords[2]);
+          bottom = parseInt(coords[3]);
 
           if (boundaries.hasOwnProperty('left')) {
             boundaries.left = Math.min(left, boundaries.left);
@@ -87,8 +99,10 @@ export default class TextBoxTool extends Component {
           width = right-left;
           height = bottom-top;
 
+          text = words[i].innerText;
+
           boxes.push((
-            <div title={words[i].innerText} style={{
+            <div title={text} style={{
               left,
               top,
               width,
@@ -97,8 +111,18 @@ export default class TextBoxTool extends Component {
               position: 'absolute'
             }}/>
           ));
+          distances.push(
+            <li data-distance={left-x_gravity*factor}>{text} {left-x_gravity*factor}</li>
+          );
         }
+
+        distances.sort((a,b) => {
+          console.log(a,b);
+          return a.props['data-distance'] < b.props['data-distance']
+        });
+
         console.log(boundaries);
+
         boxes.unshift((
           <div style={{
             left: boundaries.left*factor,
@@ -109,7 +133,30 @@ export default class TextBoxTool extends Component {
             position: 'absolute'
           }}/>
         ));
-        that.setState({boxes});
+        boxes.unshift(
+          <div style={{
+            left: x_gravity*factor,
+            top: 0,
+            width: 0,
+            height: img.height,
+            border: '1px solid red',
+            position: 'absolute'
+          }}/>
+        );
+        boxes.unshift(
+          <div style={{
+            left: words[Math.floor(words.length/2)].coords[0]*factor,
+            top: 0,
+            width: 0,
+            height: img.height,
+            border: '1px solid blue',
+            position: 'absolute'
+          }}/>
+        );
+        that.setState({
+          boxes,
+          distances
+        });
       }
     })
     .catch(function(error) {
@@ -125,6 +172,7 @@ export default class TextBoxTool extends Component {
           <img id="img" style={{width:'500px'}}/>
           {this.state.boxes}
         </div>
+        <ul>{this.state.distances}</ul>
       </div>
     )
   }
