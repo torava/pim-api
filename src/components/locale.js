ddccc0d406b83e1ba22afbca7a6cc068cfe484ad
Teemu Orava <teemu.orava@kapsi.fi>
2018-10-12 02:38:15 +0300

    Changes locale to normal class.

--- src/components/locale.js
+++ src/components/locale.js
@@ -1,20 +1,11 @@
-import React from 'react';
-
-export default class Locale extends React.Component {
-  constructor(props) {
-    super(props);
+class Locale {
+  constructor() {
+    this.currency = 'eur';
+    this.locale = 'en-us';
 
     if (typeof window !== 'undefined') {
-      this.state = {
-        currency: window.localStorage.getItem('currency'),
-        locale: window.localStorage.getItem('locale')
-      }
-    }
-    else {
-      this.state = {
-        currency: 'eur',
-        locale: 'en-us'
-      }
+      this.currency = window.localStorage.getItem('currency');
+      this.locale = window.localStorage.getItem('locale');
     }
   }
   setCurrency(currency) {
@@ -22,23 +13,19 @@ export default class Locale extends React.Component {
     if (typeof window !== 'undefined') {
       window.localStorage.setItem('currency', currency);
     }
-    this.setState({
-      currency
-    });
+    this.currency = currency;
   }
   setLocale(locale) {
     if (typeof window !== 'undefined') {
       window.localStorage.setItem('locale', locale);
     }
-    this.setState({
-      locale
-    });
+    this.locale = locale;
   }
   getCurrency() {
-    return this.state.currency;
+    return this.currency;
   }
   getLocale() {
-    return this.state.locale;
+    return this.locale;
   }
   getTranslation(field) {
     let translations = {
@@ -48,12 +35,11 @@ export default class Locale extends React.Component {
       }
     }
 
-    return (translations.hasOwnProperty(this.state.locale) &&
-           translations[this.state.locale].hasOwnProperty(field)) ?
-           translations[this.state.locale][field] :
+    return (translations.hasOwnProperty(this.locale) &&
+           translations[this.locale].hasOwnProperty(field)) ?
+           translations[this.locale][field] :
            translations['en-us'][field];
   }
-  render() {
-    return this.props.children(locale);
-  }
 }
+
+export const locale = new Locale();
