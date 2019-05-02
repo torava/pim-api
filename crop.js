bf132793a822fe1b5449761a19d6ad764b01e2c8
Teemu Orava <teemu.orava@kapsi.fi>
2019-05-02 21:48:14 +0300

    Rotate cropping

--- crop.js
+++ crop.js
@@ -3,9 +3,9 @@ somestuff: {
 blur: 9,
 threshold_area: 99,
 threshold: 15,
-post_threshold_erode_dilate: 0,
+post_threshold_erode_dilate: 25,
 canny: 3,
-close: 60,
+close: 50,
 post_canny_erode_dilate: 5
 },
 morestuff: {
@@ -33,7 +33,7 @@ post_canny_erode_dilate: 25
 }
 };
 
-let preset = presets.messy;
+let preset = presets.somestuff;
 
 let src = cv.imread('canvasInput');
 let eq = new cv.Mat();
@@ -115,14 +115,15 @@ let contours = new cv.MatVector();
     let point2 = new cv.Point(rect.x + rect.width, rect.y + rect.height);
     cv.rectangle(dst, point1, point2, rectangleColor, 2, cv.LINE_AA, 0);
 
-rotateImage(src, rotatedRect.angle);
-
 scale = 1;
     rect = new cv.Rect(Math.max(rect.x-0, 0)*scale, Math.max(rect.y-0, 0)*scale, Math.min(rect.width+0, src.cols)*scale, Math.min(rect.height+0, src.rows)*scale);
     
 let cropped = src.roi(rect);
 
-cv.imshow('canvasOutput', dst);
+cropped = rotateImage(cropped, rotatedRect.angle);
+
+
+cv.imshow('canvasOutput', cropped);
 
 
 let srcVec = new cv.MatVector();
@@ -157,6 +158,7 @@ src.delete();
 dst.delete();
 
 function rotateImage(src, rotate) {
+
     if (rotate < 0) {
         rotate = 360+rotate;
     }
@@ -173,25 +175,35 @@ function rotateImage(src, rotate) {
     }
     else if (!rotate) {}
     else {
+rotate = 45;
+let rotw = Math.abs(src.cols*Math.sin((90-rotate)*Math.PI/180))+Math.abs(src.rows*Math.sin(rotate*Math.PI/180));
+let roth = Math.abs(src.rows*Math.sin((90-rotate)*Math.PI/180))+Math.abs(src.cols*Math.sin(rotate*Math.PI/180));
+let s = new cv.Scalar(0, 0, 0, 255);
+let size = new cv.Size(src.cols, src.rows);
+
+        let dst = new cv.Mat(size, src.type());
+
+console.log(src.cols, src.rows, rotw, roth, rotw-src.cols, roth-src.rows);
+src.copyTo(dst);
+cv.copyMakeBorder(dst, dst, Math.max(roth-src.rows,0), 0, Math.max(rotw-src.cols), 0, cv.BORDER_CONSTANT, s);
+    console.log(src.cols, src.rows);
         // get rotation matrix for rotating the image around its center in pixel coordinates
-        let center = new cv.Point((src.cols-1)/2.0, (src.rows-1)/2.0);
+        let center = new cv.Point((dst.cols-1)/2.0, (dst.rows-1)/2.0);
         let rot = cv.getRotationMatrix2D(center, rotate, 1.0);
         // determine bounding rectangle, center not relevant
-        let bbox = new cv.RotatedRect(new cv.Point(), src.size(), rotate);
-        console.log(bbox);
+        let bbox = new cv.RotatedRect(new cv.Point(), dst.size(), rotate);
+
+        console.log(src, bbox);
         // adjust transformation matrix
-        rot.data[0+src.rows*2]+= bbox.size.width/2.0 - src.cols/2.0;
-        rot.data[1+src.rows*2]+= bbox.size.height/2.0 - src.rows/2.0;
+        //rot.data[3]+= bbox.size.width/2.0 - src.cols/2.0;
+        //rot.data[9]+= bbox.size.height/2.0 - src.rows/2.0;
         //rot.at<double>(0,2) += bbox.width/2.0 - src.cols/2.0;
         //rot.at<double>(1,2) += bbox.height/2.0 - src.rows/2.0;
 
-        size = new cv.Size(bbox.size.width, bbox.size.height);
+        
 
-        let dst = new cv.Mat(size);
-
-        cv.warpAffine(src, dst, rot, new cv.Size(bbox.size.width, bbox.size.height));
-
-        return dst;
+        cv.warpAffine(dst, dst, rot, size);
+return dst;
     }
-    return src;
+    return dst;
     }
