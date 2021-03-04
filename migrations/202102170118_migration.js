5d69a648b6797afc5f7f98cdafc70f1c5074057d
Teemu Orava <teemu.orava@kapsi.fi>
2021-03-04 23:25:05 +0200

    Added unique columns to avoid duplicate seeds

--- migrations/202102170118_migration.js
+++ migrations/202102170118_migration.js
@@ -2,7 +2,7 @@ const createTableIfNotExists = (knex, table, callback) => (
   knex.schema.hasTable(table).then(exists => !exists && knex.schema.createTable(table, callback))
 );
 
-exports.up = (knex, Promise) => (
+exports.up = (knex) => (
   createTableIfNotExists(knex, 'Party', table => {
     table.increments('id').primary();
     table.string('name');
@@ -13,10 +13,12 @@ exports.up = (knex, Promise) => (
     table.string('city');
     table.string('phone_number');
     table.string('email');
+    table.string('country_code');
+    table.unique(['name', 'vat', 'street_name', 'street_number', 'city', 'country_code']);
   })
   .then(() => createTableIfNotExists(knex, 'Group', table => {
     table.increments('id').primary();
-    table.string('name');
+    table.string('name').unique();
   }))
   .then(() => createTableIfNotExists(knex, 'Transaction', table => {
     table.increments('id').primary();
@@ -28,7 +30,7 @@ exports.up = (knex, Promise) => (
   }))
   .then(() => createTableIfNotExists(knex, 'Manufacturer', table => {
     table.increments('id').primary();
-    table.string('name');
+    table.string('name').unique();
     table.json('aliases');
     table.string('factory_location');
     table.string('manufacturer_location');
@@ -36,9 +38,10 @@ exports.up = (knex, Promise) => (
   }))
   .then(() => createTableIfNotExists(knex, 'Category', table => {
     table.increments('id').primary();
-    table.json('name');
+    table.jsonb('name');
     table.specificType('aliases', 'text ARRAY');
     table.integer('parentId').unsigned().references('id').inTable('Category');
+    table.unique(['name', 'parentId']);
   }))
   .then(() => createTableIfNotExists(knex, 'Product', table => {
     table.increments('id').primary();
@@ -75,12 +78,13 @@ exports.up = (knex, Promise) => (
   }))
   .then(() => createTableIfNotExists(knex, 'Attribute', table => {
     table.increments('id').primary();
-    table.json('name');
+    table.jsonb('name');
     table
       .integer('parentId')
       .unsigned()
       .references('id')
       .inTable('Attribute');
+    table.unique(['name', 'parentId']);
   }))
   .then(() => createTableIfNotExists(knex, 'CategoryContribution', table => {
     table.increments('id').primary();
@@ -96,6 +100,7 @@ exports.up = (knex, Promise) => (
       .unsigned()
       .references('id')
       .inTable('Category');
+    table.unique(['categoryId', 'contributionId']);
   }))
   .then(() => createTableIfNotExists(knex, 'ProductAttribute', table => {
     table.increments('id').primary();
@@ -112,6 +117,7 @@ exports.up = (knex, Promise) => (
       .references('id')
       .inTable('Attribute')
       .onDelete('CASCADE');
+    table.unique(['value', 'unit', 'productId', 'attributeId']);
   }))
   .then(() => createTableIfNotExists(knex, 'CategoryAttribute', table => {
     table.increments('id').primary();
@@ -129,6 +135,7 @@ exports.up = (knex, Promise) => (
       .references('id')
       .inTable('Attribute')
       .onDelete('CASCADE');
+    table.unique(['value', 'unit', 'categoryId', 'attributeId']);
   }))
   .then(() => createTableIfNotExists(knex, 'Conversion', table => {
     table.increments('id').primary();
@@ -145,6 +152,7 @@ exports.up = (knex, Promise) => (
     table.string('country_code');
     table.string('publication_date');
     table.string('publication_url');
+    table.unique(['name', 'authors', 'publication_url']);
   }))
   .then(() => createTableIfNotExists(knex, 'CategoryAttributeSource', table => {
     table.increments('id').primary();
@@ -162,6 +170,7 @@ exports.up = (knex, Promise) => (
       .unsigned()
       .references('id')
       .inTable('Source');
+    table.unique(['attributeId', 'sourceId']);
   }))
 );
 
