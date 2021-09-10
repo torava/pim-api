2d4f3eeb8071b5324e228ad181d9cf7fd56c7ffa
Teemu Orava <teemu.orava@kapsi.fi>
2021-09-10 13:05:45 +0300

    Uses production for knex by default. Changed database. v0.0.27

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
       host: process.env.POSTGRES_HOST || 'localhost',
       user: process.env.POSTGRES_USER || 'postgres',
       password: process.env.POSTGRES_PASSWORD || 'password',
-      database: process.env.POSTGRES_DB || 'bookkeepr',
+      database: process.env.POSTGRES_DB || 'product-api',
       port: Number(process.env.POSTGRES_PORT) || 5432
       //filename: './example.db'
     }
@@ -15,16 +15,13 @@ module.exports = {
 
   production: {
     client: 'postgresql',
+    useNullAsDefault: true,
     connection: {
       host: process.env.POSTGRES_HOST || 'localhost',
       user: process.env.POSTGRES_USER || 'postgres',
       password: process.env.POSTGRES_PASSWORD || 'password',
-      database: process.env.POSTGRES_DB || 'bookkeepr',
+      database: process.env.POSTGRES_DB || 'product-api',
       port: Number(process.env.POSTGRES_PORT) || 5432
-    },
-    pool: {
-      min: 2,
-      max: 10
     }
   }
 
