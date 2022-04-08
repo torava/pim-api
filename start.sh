6bc76dc40230b65a47c57f67367c939ab288c5d6
Teemu Orava <teemu.orava@kapsi.fi>
2022-04-08 12:40:15 +0300

    Fixed if condition v1.0.9

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ ls -ltr &&
 npm config set '//gitlab.com/api/v4/projects/32804813/packages/npm/:_authToken' "${NPM_TOKEN}"
 npm ci &&
 NODE_OPTIONS=--max_old_space_size=8192 npm run build &&
-if [ "$MIGRATE" == "true" ] ; then
+if "$MIGRATE" == "true" ; then
 npm run knex migrate:latest &&
 npm run knex seed:run ;
 else echo Migration skipped ; fi &&
