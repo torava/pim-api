2a82edd3754735d69224c8d006f5b71ff864d146
Teemu Orava <teemu.orava@kapsi.fi>
2022-08-17 20:35:11 +0300

    Writes GHG to xlsx 0.5h

--- src/utils/getDiaryExcelFineli.ts
+++ src/utils/getDiaryExcelFineli.ts
@@ -9,6 +9,7 @@ import { Model } from 'objection';
 import knexConfig from '../../knexfile';
 import Category from '../models/Category';
 import Attribute from '../models/Attribute';
+import { min } from 'lodash';
 
 export const getDiaryExcelFineli = async (
   filename: string,
@@ -19,18 +20,47 @@ export const getDiaryExcelFineli = async (
   const workbook = new Excel.Workbook();
   await workbook.xlsx.readFile(filename);
   const worksheet = workbook.worksheets[0];
-  let totalMin = 0,
-      totalMax = 0,
-      totalMeasure = 0;
+  let totalMealMin = 0,
+      totalMealMax = 0,
+      totalDayMin = 0,
+      totalDayMax = 0,
+      totalMealMeasure = 0,
+      totalDayMeasure = 0;
+  const headerRow = worksheet.getRow(1);
+  headerRow.getCell(65).value = 'Min. GHG (kgCO₂e)';
+  headerRow.getCell(66).value = 'Max. GHG (kgCO₂e)';
+  headerRow.getCell(67).value = 'Min. GHG/weight (kgCO₂e/kg)';
+  headerRow.getCell(68).value = 'Max. GHG/weight (kgCO₂e/kg)';
   worksheet.eachRow((row) => {
     //const row = worksheet.getRows(40, 1)[0];
     const food = row.getCell(4).value;
     const unit = row.getCell(8).value;
+    const minGhgCell = row.getCell(65);
+    const maxGhgCell = row.getCell(66);
+    const minGhgPerMeasureCell = row.getCell(67);
+    const maxGhgPerMeasureCell = row.getCell(68);
     if (!food) {
-      console.log('total', totalMin, totalMax, totalMeasure);
-      totalMin = 0;
-      totalMax = 0;
-      totalMeasure = 0;
+      if (!totalMealMin) {
+        console.log('total day', totalDayMin, totalDayMax, totalDayMeasure);
+        minGhgCell.value = totalDayMin;
+        maxGhgCell.value = totalDayMax || totalDayMin;
+        minGhgPerMeasureCell.value = totalDayMin/totalDayMeasure;
+        maxGhgPerMeasureCell.value = (totalDayMax || totalDayMin)/totalDayMeasure;
+        totalDayMin = 0;
+        totalDayMax = 0;
+        totalDayMeasure = 0;
+      }
+      console.log('total meal', totalMealMin, totalMealMax, totalMealMeasure);
+      minGhgCell.value = totalMealMin;
+      maxGhgCell.value = totalMealMax || totalMealMin;
+      minGhgPerMeasureCell.value = totalMealMin/totalMealMeasure;
+      maxGhgPerMeasureCell.value = (totalMealMax || totalMealMin)/totalMealMeasure;
+      totalDayMin+= totalMealMin;
+      totalDayMax+= totalMealMax;
+      totalDayMeasure+= totalMealMeasure;
+      totalMealMin = 0;
+      totalMealMax = 0;
+      totalMealMeasure = 0;
     } else {
       const category = categories.find(
         (category) => category.name?.[locale] === food
@@ -57,14 +87,19 @@ export const getDiaryExcelFineli = async (
           categoryAttributes[1]?.type,
           measure
         );
-        totalMin+= categoryAttributes[0]?.value || 0;
-        totalMax+= categoryAttributes[1]?.value || categoryAttributes[0]?.value || 0;
-        totalMeasure+= measure;
+        minGhgCell.value = categoryAttributes[0]?.value;
+        maxGhgCell.value = categoryAttributes[1]?.value || categoryAttributes[0]?.value;
+        minGhgPerMeasureCell.value = categoryAttributes[0]?.value/measure;
+        maxGhgPerMeasureCell.value = (categoryAttributes[1]?.value || categoryAttributes[0]?.value)/measure;
+        totalMealMin+= categoryAttributes[0]?.value || 0;
+        totalMealMax+= categoryAttributes[1]?.value || categoryAttributes[0]?.value || 0;
+        totalMealMeasure+= measure;
       } else {
         console.log(food, "not found");
       }
     }
   });
+  await workbook.xlsx.writeFile(`${filename}_ghg.xlsx`);
 };
 
 // Initialize knex.
