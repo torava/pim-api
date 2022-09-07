8b55551b0efa008e835855de916e8d939e91b218
Teemu Orava <teemu.orava@kapsi.fi>
2022-09-07 17:55:30 +0300

    Removed setupTests

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,6 @@ module.exports = {
   setupFiles: [
     '<rootDir>/src/__mocks__/setupJestMock.js'
   ],
-  setupFilesAfterEnv: [
-    '<rootDir>/src/setupTests.ts'
-  ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/fileMock.js',
     '\\.(css|less|scss)$': '<rootDir>/src/__mocks__/styleMock.js'
