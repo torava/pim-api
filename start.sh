2ab93015a812e94d69b7eb9f6192f432c69ffe94
Teemu Orava <teemu.orava@kapsi.fi>
2026-01-29 15:10:12 +0200

    Change max old space size to 8192

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 ls -ltr &&
 npm config set '//gitlab.com/api/v4/projects/32804813/packages/npm/:_authToken' "${NPM_TOKEN}"
-NODE_OPTIONS=--max_old_space_size=4096 npm ci &&
-NODE_OPTIONS=--max_old_space_size=4096 npm run build &&
+NODE_OPTIONS=--max_old_space_size=8192 npm ci &&
+NODE_OPTIONS=--max_old_space_size=8192 npm run build &&
 if [ "$MIGRATE" == "true" ]
 then
 npm run knex migrate:latest --env production &&
