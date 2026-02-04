fae3e02a45f076f52817c5a83385f074396bff7e
Teemu Orava <teemu.orava@kapsi.fi>
2026-02-04 18:41:48 +0200

    Fix migration script v1.1.15

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ ls -ltr &&
 npm config set '//gitlab.com/api/v4/projects/32804813/packages/npm/:_authToken' "${NPM_TOKEN}"
 npm ci &&
 NODE_OPTIONS=--max_old_space_size=1024 npm run build &&
-if [ "$MIGRATE" == "true" ]
+if [ "$MIGRATE" = "true" ]
 then
 npm run knex migrate:latest --env production &&
 npm run knex seed:run --env production
