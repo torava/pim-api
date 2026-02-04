dae685bafd0a0c0769854aa38e601263c7273951
Teemu Orava <teemu.orava@kapsi.fi>
2026-02-04 20:01:42 +0200

    Add NODE_ENV v1.1.17

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ npm ci &&
 NODE_OPTIONS=--max_old_space_size=1024 npm run build &&
 if [ "$MIGRATE" = "true" ]
 then
-npm run knex migrate:latest --mode production &&
-npm run knex seed:run --mode production
+NODE_ENV=production npm run knex migrate:latest &&
+NODE_ENV=production npm run knex seed:run
 else
 echo Migration skipped
 fi &&
