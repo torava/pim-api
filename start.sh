070da05c6cf6f2e1f3c4d0d380aa44f9f2f918f0
Teemu Orava <teemu.orava@kapsi.fi>
2026-02-05 13:39:17 +0200

    Add NODE_ENV v1.1.22

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 ls -ltr &&
 npm config set '//gitlab.com/api/v4/projects/32804813/packages/npm/:_authToken' "${NPM_TOKEN}"
 npm ci &&
-NODE_OPTIONS=--max_old_space_size=2048 npm run build &&
+NODE_ENV=production NODE_OPTIONS=--max_old_space_size=2048 npm run build &&
 if [ "$MIGRATE" = "true" ]
 then
 NODE_ENV=production NODE_OPTIONS=--max_old_space_size=2048 npm run knex migrate:latest &&
