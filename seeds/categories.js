844179843e117d9c0cd1db2cbb0ea586527c97c0
Teemu Orava <teemu.orava@kapsi.fi>
2026-03-16 14:59:11 +0200

    Run prettier

--- seeds/categories.js
+++ seeds/categories.js
@@ -12,9 +12,9 @@ import RecommendationSource from '../src/models/RecommendationSource';
 const SEED_SUFFIX = process.env.SEED_SUFFIX || '';
 const DELIMITER = process.env.DELIMITER || ';';
 
-exports.seed = async knex => {
+exports.seed = async (knex) => {
   Model.knex(knex);
-  
+
   try {
     if (!SEED_SUFFIX) {
       await getExternalCategoriesFineli('seeds/Fineli_Rel20__74_ravintotekij__');
@@ -25,7 +25,7 @@ exports.seed = async knex => {
 
   const sourcesCsv = fs.readFileSync(`${__dirname}/sources${SEED_SUFFIX}.csv`, 'utf8');
   const sources = getEntitiesFromCsv(sourcesCsv, { delimiter: DELIMITER });
-  
+
   const categoriesCsv = fs.readFileSync(`${__dirname}/categories${SEED_SUFFIX}.csv`, 'utf8');
   const categories = getEntitiesFromCsv(categoriesCsv, { delimiter: DELIMITER });
 
@@ -38,11 +38,10 @@ exports.seed = async knex => {
   }
   try {
     const categoryParents = await getCategoryParentsFromCsv(categories);
-    await Category.query()
-    .upsertGraph(categoryParents, {
+    await Category.query().upsertGraph(categoryParents, {
       noDelete: true,
       relate: true,
-      allowRefs: true
+      allowRefs: true,
     });
   } catch (error) {
     console.error('error while adding CSV category parents', error);
@@ -52,30 +51,35 @@ exports.seed = async knex => {
     const attributes = getLeafEntities(await Attribute.query());
 
     const recommendationsCsv = fs.readFileSync(`${__dirname}/recommendations.csv`, 'utf8');
-    getEntitiesFromCsv(recommendationsCsv, { delimiter: DELIMITER }).filter(entity => entity.minValue || entity.maxValue).forEach(async entity => {
-      const attributeId = attributes.find(attribute => attribute.name['en-US'].toLocaleLowerCase() === entity['attribute.name["en-US"]'].toLocaleLowerCase())?.id;
-      const sourceId = sourceRecordIdMap[entity.sourceId]?.id || undefined;
-      delete entity['attribute.name["en-US"]'];
-      delete entity.sourceId;
-      try {
-        const recommendation = await Recommendation.query().insert({
-          ...entity,
-          attributeId,
-          minValue: parseFloat(entity.minValue.replace(',', '.')) || undefined,
-          maxValue: parseFloat(entity.maxValue.replace(',', '.')) || undefined,
-          minimumAge: parseInt(entity.minimumAge) || undefined,
-          maximumAge: parseInt(entity.maximumAge) || undefined,
-          weight: parseFloat(entity.weight.replace(',', '.')) || undefined,
-          pav: parseInt(entity.pav) || undefined,
-          pal: parseFloat(entity.pal.replace(',', '.')) || undefined
-        });
-        await RecommendationSource.query().insert({
-          recommendationId: recommendation.id,
-          sourceId
-        });
-      } catch (error) {
-        console.error('error while adding recommendations', error);
-      }
-    });
+    getEntitiesFromCsv(recommendationsCsv, { delimiter: DELIMITER })
+      .filter((entity) => entity.minValue || entity.maxValue)
+      .forEach(async (entity) => {
+        const attributeId = attributes.find(
+          (attribute) =>
+            attribute.name['en-US'].toLocaleLowerCase() === entity['attribute.name["en-US"]'].toLocaleLowerCase()
+        )?.id;
+        const sourceId = sourceRecordIdMap[entity.sourceId]?.id || undefined;
+        delete entity['attribute.name["en-US"]'];
+        delete entity.sourceId;
+        try {
+          const recommendation = await Recommendation.query().insert({
+            ...entity,
+            attributeId,
+            minValue: parseFloat(entity.minValue.replace(',', '.')) || undefined,
+            maxValue: parseFloat(entity.maxValue.replace(',', '.')) || undefined,
+            minimumAge: parseInt(entity.minimumAge) || undefined,
+            maximumAge: parseInt(entity.maximumAge) || undefined,
+            weight: parseFloat(entity.weight.replace(',', '.')) || undefined,
+            pav: parseInt(entity.pav) || undefined,
+            pal: parseFloat(entity.pal.replace(',', '.')) || undefined,
+          });
+          await RecommendationSource.query().insert({
+            recommendationId: recommendation.id,
+            sourceId,
+          });
+        } catch (error) {
+          console.error('error while adding recommendations', error);
+        }
+      });
   }
 };
